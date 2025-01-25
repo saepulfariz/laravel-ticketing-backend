ff4e2933e7b1ee8ed423554cc3baacece00c7d04
saepulfariz <saepulfariz108@gmail.com>
2025-01-25 08:11:02 +0700

    add install api and add api register

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Google_Client;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        // {
+        //     "name" :"admin",
+        //     "email" :"admin@gmail.com",
+        //     "password" : "123",
+        //     "password_confirmation" : "123"
+        //   }
+
+        // create users
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User created successfully',
+            'data' => $user
+        ], 201);
+    }
+}
