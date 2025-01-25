768a54cee4c09710f7cf57c5dee939e6984b22dc
saepulfariz <saepulfariz108@gmail.com>
2025-01-25 08:26:26 +0700

    add filter canAccessPanel filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
@@ -45,5 +48,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() == 'admin') {
+            // jika email nya @admin.com bisa login ke panel dashboard
+            return str_ends_with($this->email, '@admin.com');
+        }
     }
 }
